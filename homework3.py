57807830109d5ed26c791b3c810c23c60a4a9a32
Abhishek Anand <anand1@uw.edu>
2018-04-16 21:31:55 -0700

    Adding homework3.py test_homework3.py .gitignore

---
+++ homework3.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import sqlite3
+import sys
+
+
+def create_dataframe(arg1):
+    connection = sqlite3.connect(arg1)  # Conncting to the sqlite3 data base
+    df = pd.read_sql_query("""SELECT video_id, category_id,'us' AS language FROM USvideos UNION
+                              SELECT video_id, category_id,'ca' AS language FROM CAvideos UNION
+                              SELECT video_id, category_id,'de' AS language FROM DEvideos UNION
+                              SELECT video_id, category_id,'fr' AS language FROM FRvideos UNION
+                              SELECT video_id, category_id,'gb' AS language FROM GBvideos""", connection)
+    return df
+
+
+def checkColumnName(df):
+    columnName = list(df)  # https://stackoverflow.com/a/19483025
+    numberofColumn = len(columnName)
+    if(numberofColumn == 3):
+        if set(['video_id', 'language', 'category_id']).issubset(
+                set(columnName)):  # https://stackoverflow.com/a/6159356
+            return True
+        else:
+            return False
+
+# There are at least 10 rows in the DataFrame.
+def checkRowSize(df):
+    Count_Row = df.shape[0]  # https://stackoverflow.com/a/35523946
+    if(Count_Row >= 10):
+        return True
+    else:
+        return False
+
+
+def checkKey(df):
+    Count_Row = df.shape[0]
+    # https://stackoverflow.com/a/19378497
+    video_idset = len(set(df['video_id'].map(str) + df["language"].map(str)))
+    #print(Count_Row)  # 35950
+    #print(video_idset)  # 30697 (without concatenate )  35920 with concatenate
+    if (Count_Row == video_idset):
+        return True
+    else:
+        return False          
---
+++ test_homework3.py
@@ -0,0 +1,24 @@
+import unittest
+from homework3 import create_dataframe
+from homework3 import checkColumnName
+from homework3 import checkRowSize
+from homework3 import checkKey
+
+class create_dataframe_test(unittest.TestCase):
+    """Tests for `Creation of dataframe`."""
+
+    def test_checkColumnName(self):
+        """Check if column Name is same """
+        self.assertTrue(checkColumnName(create_dataframe("class.db")))  
+    
+    def test_checkRowSize(self):
+        """Check Row Size """
+        self.assertTrue(checkRowSize(create_dataframe("class.db"))) 
+    
+    def test_checkKey(self):
+        """Check Key """
+        self.assertFalse(checkKey(create_dataframe("class.db")))   
+
+if __name__ == '__main__':
+    unittest.main()
+
